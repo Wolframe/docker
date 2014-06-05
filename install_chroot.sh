b3bdf7d33c8a9b65503703fc62c6b77b1f57bf6b
Andreas Baumann <abaumann@yahoo.com>
2014-06-05 17:23:54 +0200

    added 32-bit Centos 6 added a non-working Centos 5

--- install_chroot.sh
+++ install_chroot.sh
@@ -4,7 +4,7 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin:${PATH}
 
 # ArchLinux
 
-CHROOT_DIR=arch
+CHROOT_DIR=archlinux-x86_64
 
 mkdir $CHROOT_DIR
 
@@ -15,11 +15,55 @@ sed -i 's/^SigLevel.*/SigLevel = Never/g' $CHROOT_DIR/etc/pacman.conf
 systemd-nspawn -D $CHROOT_DIR
 chroot $CHROOT_DIR
 
-tar --numeric-owner -C $CHROOT_DIR -cf - . | docker import - wolframe/archlinux-64-base:201406
+tar --numeric-owner -C $CHROOT_DIR -cf - . | docker import - wolframe/archlinux-x86_64-base:201406
 
-# Centos
+# Centos 5
 
-CHROOT_DIR=centos
+CHROOT_DIR=centos5
+
+mkdir $CHROOT_DIR
+
+mkdir -p $CHROOT_DIR/var/lib/rpm
+
+rpm -v --rebuilddb --root=`pwd`/$CHROOT_DIR
+
+wget http://mirror.centos.org/centos/5.10/os/x86_64/CentOS/centos-release-5-10.el5.centos.x86_64.rpm
+
+rpm -v -i --root=`pwd`/$CHROOT_DIR --nodeps centos-release-5-10.el5.centos.x86_64.rpm
+
+rm -f centos-release-5-10.el5.centos.x86_64.rpm
+
+wget http://mirror.centos.org/centos/5.10/os/x86_64/RPM-GPG-KEY-CentOS-5
+
+rpm -v --root=`pwd`/$CHROOT_DIR --import RPM-GPG-KEY-CentOS-5
+
+rm RPM-GPG-KEY-CentOS-5
+
+yum --installroot=`pwd`/$CHROOT_DIR install -y yum
+yum --installroot=`pwd`/$CHROOT_DIR install -y coreutils procps net-tools wget
+
+# rebuild the RPM database with the rpm inside the chroot
+cd $CHROOT_DIR/var/lib/rpm
+mv Packages Packages-ORIG
+db_dump Packages-ORIG 
+chroot $CHROOT_DIR /bin/rpm -v --rebuilddb
+
+cd /var/lib/rpm
+     mv Packages Packages-ORIG
+     db_dump-4.6.18 Packages-ORIG | db_load-4.5.20 Packages
+    rpm --rebuilddb
+    rpm -qa
+
+
+
+systemd-nspawn -D $CHROOT_DIR 
+chroot $CHROOT_DIR
+
+tar --numeric-owner -C $CHROOT_DIR -cf - . | docker import - wolframe/centos-64-base:6.5
+
+# Centos 6 (64-bit)
+
+CHROOT_DIR=centos6-x86_64
 
 mkdir $CHROOT_DIR
 
@@ -48,7 +92,42 @@ chroot $CHROOT_DIR /bin/rpm -v --rebuilddb
 systemd-nspawn -D $CHROOT_DIR 
 chroot $CHROOT_DIR
 
-tar --numeric-owner -C $CHROOT_DIR -cf - . | docker import - wolframe/centos-64-base:6.5
+tar --numeric-owner -C $CHROOT_DIR -cf - . | docker import - wolframe/centos6-x86_64-base:6.5
+ 
+# Centos 6 (32-bit)
+
+CHROOT_DIR=centos6-i386
+
+mkdir $CHROOT_DIR
+
+mkdir -p $CHROOT_DIR/var/lib/rpm
+
+rpm -v --rebuilddb --root=`pwd`/$CHROOT_DIR
+
+wget http://mirror.centos.org/centos/6.5/os/i386/Packages/centos-release-6-5.el6.centos.11.1.i686.rpm
+
+rpm -v -i --root=`pwd`/$CHROOT_DIR --nodeps centos-release-6-5.el6.centos.11.1.i686.rpm 
+
+rm -f centos-release-6-5.el6.centos.11.1.i686.rpm
+
+wget http://mirror.centos.org/centos/6.5/os/i386/RPM-GPG-KEY-CentOS-6
+
+rpm -v --root=`pwd`/$CHROOT_DIR --import RPM-GPG-KEY-CentOS-6 
+
+rm RPM-GPG-KEY-CentOS-6
+
+linux32 yum --installroot=`pwd`/$CHROOT_DIR install -y yum
+linux32 yum --installroot=`pwd`/$CHROOT_DIR install -y coreutils procps net-tools wget
+
+# rebuild the RPM database with the rpm inside the chroot
+linux32 chroot $CHROOT_DIR /bin/rpm -v --rebuilddb
+
+cp /etc/resolv.conf $CHROOT_DIR/etc/
+
+systemd-nspawn --personality=x86 -D $CHROOT_DIR 
+linux32 chroot $CHROOT_DIR
+
+tar --numeric-owner -C $CHROOT_DIR -cf - . | docker import - wolframe/centos6-i386-base:6.5
 
 # Debian
 
